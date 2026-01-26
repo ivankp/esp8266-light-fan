22e5cc63d990bc1eca743d56cbd3a328fce5929a
ivankp <ivan.pogrebnyak@gmail.com>
2026-01-25 23:08:46 -0500

    started writing tests

---
+++ README.md
@@ -0,0 +1,8 @@
+# TODO
+
+* Write tests.
+* Revert to storing only one set of WiFi credentials.
+* Function to reset nvs.
+* User can set desired station and AP IP address. Apply on reboot.
+* Debug mode:
+  * Turn on LED if AP mode is on.
---
+++ tests/test-set.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+SSID='light-and-fan'
+
+nmcli device wifi connect "$SSID" password 'automation'
+
+nmcli dev status \
+| awk ''' {
+  if ($2 == "wifi") {
+    print $0
+    if ($4 == "'''"$SSID"'''" && $3 == "connected") {
+      e = 1
+    }
+    exit
+  }
+} END { exit !e }'''
+
+if [ $? -ne 0 ]; then
+  echo "Failed to connect to $SSID"
+  exit 1
+fi
